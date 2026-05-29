dd32b2ca26cd19f33a169354ad16c850ab6c8b5a
godzaroky <gon24170@uvg.edu.gt>
2026-05-28 22:21:42 -0600

    fix: agregar ThemeProvider y StorageProvider en main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
+import './index.css';
+import App from './App';
+import { StorageProvider } from './context/StorageContext';
+import { ThemeProvider } from './context/ThemeContext';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <StorageProvider>
+        <App />
+      </StorageProvider>
+    </ThemeProvider>
   </StrictMode>
 );
